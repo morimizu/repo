2af03180d42a6ac63ededfa369b5b8671706e9e6
morimizu <neb_bbc@yahoo.com>
2019-09-30 11:43:40 -0300

    adding bootstrap test

---
+++ SoloarSystemTest/assets/javascripts.js
@@ -0,0 +1,10 @@
+$('.closeall').click(function(){
+  $('.panel-collapse.in')
+    .collapse('hide');
+});
+$('.openall').click(function(){
+  $('.panel-collapse:not(".in")')
+    .collapse('show');
+});
+$('.panel-collapse.in')
+.collapse('hide');
